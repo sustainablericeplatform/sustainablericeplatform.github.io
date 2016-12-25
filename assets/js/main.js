735ed883f4bd09ac15b20fc05eb262d999aa3df7
instigators <ideas@instigators.io>
2016-12-26 00:34:29 +0800

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.slideshow').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+    });
+
+    // Get Involved section
+    $('.company-title').on('click touch', function(event) {
+        event.preventDefault();
+
+        $(this).closest('li').find('.company-details-wrap').toggleClass('hidden');
+    })
+});
