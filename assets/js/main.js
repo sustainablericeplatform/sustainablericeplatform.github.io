e865ef7934636a4d45ca9ae2e02e01d42fa76b9e
Dominic Tuazon <dominic@instigators.io>
2017-03-22 01:07:22 +0800

    Fixed responsiveness and changed fonts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-    $('.slideshow').slick({
-        autoplay: true,
-        arrows: false,
-        dots: true,
-        speed: 1000,
-    });
+    if($(window).width() > 420) {
+        $('.slideshow').slick({
+            autoplay: true,
+            arrows: false,
+            dots: true,
+            speed: 1000,
+        });
+    }
+
 
     // Get Involved section
     $('.company-title').on('click touch', function(event) {
